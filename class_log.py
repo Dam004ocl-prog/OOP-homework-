fb019129aed336d8a428fabe727968f2caee006c
Evgenii Vasendin <zvasendin@gmail.com>
2025-05-05 13:39:27 +0300

    Task 3 completed

--- class_log.py
+++ class_log.py
@@ -20,16 +20,48 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        avg = sum(sum(g) for g in self.grades.values()) / sum(len(g) for g in self.grades.values()) \
+              if self.grades else 0
+        return (f"Имя: {self.name}\nФамилия: {self.surname}\n"
+                f"Средняя оценка: {avg:.1f}\nКурсы: {', '.join(self.courses_in_progress)}\n"
+                f"Завершено: {', '.join(self.finished_courses) or 'нет'}")
+
+    def __eq__(self, other):
+        return self._avg() == other._avg()
+    def __lt__(self, other):
+        return self._avg() < other._avg()
+    def _avg(self):
+        return sum(map(sum, self.grades.values())) / sum(map(len, self.grades.values())) if self.grades else 0
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def add_grade(self, course, grade):
+        self.grades.setdefault(course, []).append(grade)
+    def __str__(self):
+        avg = sum(sum(g) for g in self.grades.values()) / sum(len(g) for g in self.grades.values()) \
+               if self.grades else 0
+        return super().__str__() + f"\nСредняя оценка: {avg:.1f}"
+
+    def __eq__(self, other):
+        return self._avg() == other._avg()
+    def __lt__(self, other):
+        return self._avg() < other._avg()
+    def _avg(self):
+        return sum(map(sum, self.grades.values())) / sum(map(len, self.grades.values())) if self.grades else 0
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -59,5 +91,6 @@ reviewer.rate_hw(student, 'Python', 9)
 student.rate_lecturer(lecturer, 'Python', 10)
 student.rate_lecturer(lecturer, 'Python', 9)
 
-print("Оценки студента:", student.grades)
-print("Оценки лектора:", lecturer.grades)
+print(lecturer)
+print(student)
+print(lecturer > Lecturer("Clarice", "Lecter"))
